4fff7848f1fe75ace40c31facc22094e1c0e7b63
y-arjun-y <arjyadav7@gmail.com>
2022-02-01 16:30:10 +0400

    changed icons for x-risks

--- app.py
+++ app.py
@@ -90,30 +90,30 @@ app.clientside_callback(
     function (value) {
             document.getElementById("x-risks-output").innerHTML = "<h3>Try clicking a cell!</h3>"
             if (value["row"] == 0) {
-                document.getElementById("x-risks-img").src = "/assets/x-risks-imgs/president.png";
+                document.getElementById("x-risks-img").src = "/assets/x-risks-imgs/landmark-solid.svg";
                 document.getElementById("x-risks-output").innerHTML = "<h1>1 in 1,000,000</h1><p>is approximately the odds of any one of the next 24 babies born in the U.S. becoming the President.</p>"
             } 
             else if (value["row"] == 1 || value["row"] == 3 || value["row"] == 7) {
-                document.getElementById("x-risks-img").src = "/assets/x-risks-imgs/fourclover.png";
+                document.getElementById("x-risks-img").src = "/assets/x-risks-imgs/clover-solid.svg";
                 document.getElementById("x-risks-output").innerHTML = "<h1>1 in 10,000</h1><p>is approximately the odds of finding a four-leaf clover.</p>"
             } else if (value["row"] == 2) {
-                document.getElementById("x-risks-img").src = "/assets/x-risks-imgs/baseball.png";
+                document.getElementById("x-risks-img").src = "/assets/x-risks-imgs/baseball-solid.svg";
                 document.getElementById("x-risks-img").setAttribute("class", "baseball");
                 document.getElementById("x-risks-output").innerHTML = "<h1>1 in 1,000,000,000</h1><p>is approximately the odds of catching two foul-bowls in a baseball game in a row.</p>"
             } else if (value["row"] == 4 || value["row"] == 5 || value["row"] == 6) {
-                document.getElementById("x-risks-img").src = "/assets/x-risks-imgs/february.png";
+                document.getElementById("x-risks-img").src = "/assets/x-risks-imgs/cake-candles-solid.svg";
                 document.getElementById("x-risks-output").innerHTML = "<h1>1 in 1,000</h1><p>is approximately the odds of being born on February 29th.</p>"
             } else if (value["row"] == 8 || value["row"] == 10) {
-            document.getElementById("x-risks-img").src = "/assets/x-risks-imgs/snake-eyes.png";
+            document.getElementById("x-risks-img").src = "/assets/x-risks-imgs/dice-solid.svg";
                 document.getElementById("x-risks-output").innerHTML = "<h1>1 in 30</h1><p>is approximately the odds of getting snake-eyes (rolling two ones) on two die.</p>"
             } else if (value["row"] == 9) {
-                document.getElementById("x-risks-img").src = "/assets/x-risks-imgs/phone.png";
+                document.getElementById("x-risks-img").src = "/assets/x-risks-imgs/phone-solid.svg";
                 document.getElementById("x-risks-output").innerHTML = "<h1>1 in 10</h1><p>is the odds of getting the first digit of a phone number correctly.</p>"
             } else if (value["row"] == 11) {
-                document.getElementById("x-risks-img").src = "/assets/x-risks-imgs/mask.png";
+                document.getElementById("x-risks-img").src = "/assets/x-risks-imgs/mask-face-solid.svg";
                 document.getElementById("x-risks-output").innerHTML = "<h1>1 in 50</h1><p>is approximately the odds of guessing a person had COVID-19 in England in Oct 2021.</p>"
             } else if (value["row"] == 12 || value["row"] == 13) {
-                document.getElementById("x-risks-img").src = "/assets/x-risks-imgs/dice.png";
+                document.getElementById("x-risks-img").src = "/assets/x-risks-imgs/dice-solid.svg";
                 document.getElementById("x-risks-output").innerHTML = "<h1>1 in 6</h1><p>is the odds of getting 1 on a dice roll.</p>"
             }
         }
